b2f90382fd80f46dc8df92e8f1e04d9ae159c412
Gustav <gmkarto@iddqd.no>
2016-11-02 22:57:38 +0100

    a lot of updates add message add timestamp

--- Scripts/Controllers/ChannelController.js
+++ Scripts/Controllers/ChannelController.js
@@ -1,39 +1,66 @@
 ﻿(function () {
 	var app = angular.module("lerniaChat");
-	var channelController = function ($scope, repository) {
+	var channelController = function ($scope, repository,$interval) {
 		$scope.channels = null;
 		$scope.messages = null;
 		$scope.name = null;
-		var getAllChannels = function() {
-			repository.getAllChannels()
-				.then(onChannelLoad,onError);
-		};
-		var onChannelLoad = function (response) {
-		    $scope.channels = response;
+		$scope.countdown = 1;
+		$scope.activeChannel = null;
+		$scope.author = null;
+		$scope.body = null;
 
+		var onChannelLoad = function (response) {
+			$scope.channels = response;
 		};
 		var onError = function (response) {
-		    // do error shit in here ty great job many 
-		};  
+		// do error shit in here ty great job many 
+		};
+
+		var getAllChannels = function () {
+			repository.getAllChannels()
+				.then(onChannelLoad, onError);
+		};
+
+		var poll = function () {
+			getAllChannels();
+			if ($scope.activeChannel) {
+				$scope.getMessages($scope.activeChannel);
+			}
+		};
 		$scope.getMessages = function(id) {
 			repository.getMessages(id)
 				.then(function (response) {
+					$scope.activeChannel = id;
 					$scope.messages = response.messages;
 				});
 		};
 		$scope.addChannel = function(name) {
-			repository.addChannel(name);
-			getAllChannels();
-
-		};
-		$scope.deleteChannel = function(id) {
-			repository.deleteChannel(id).then(function() {
+			repository.addChannel(name).then(function(){
 				getAllChannels();
 			});
-
 		};
-		getAllChannels();
+		$scope.deleteChannel = function(id) {
+			repository.deleteChannel(id)
+			.then(function() {
+				getAllChannels();
+				});
+		};
+		$scope.addMessage = function(id, author, body) {
+			repository.addMessage(id, author, body)
+				.then(function() {
+					getAllChannels();
+					$scope.getMessages(id);
+					$scope.body = null;
+				});
+		};
+		$scope.formatTime = function (time) {
+			var date = new Date(time);
+			var formatedtime = "["+date.getHours() + ":" + date.getMinutes()+"]";
+			return formatedtime;
+		};
+		poll();
+
 
 	};
-	app.controller("ChannelController", ["$scope","repository", channelController]);
+	app.controller("ChannelController", ["$scope","repository","$interval", channelController]);
 })();
--- Scripts/Services/repository.js
+++ Scripts/Services/repository.js
@@ -1,5 +1,6 @@
 ﻿(function() {
-	var repository = function($http,$q) {
+	var repository = function ($http) {
+		var repositoryurl = "http://dummyapi.kodalagom.se/api/";
 		var getAllChannels = function() {
 			return $http.get("http://dummyapi.kodalagom.se/api/channels")
 				.then(function(response) {
@@ -17,30 +18,39 @@
 				'name': name,
 				'messages': []
 			};
-			$http.post("http://dummyapi.kodalagom.se/api/channels", data)
+			return $http.post("http://dummyapi.kodalagom.se/api/channels", data)
 				.then(
 					function(response) {
 						return response.data;
-					},
-					function(response) {
-
 					}
 				);
 		};
 		var deleteChannel = function (id) {
-			$http.delete("http://dummyapi.kodalagom.se/api/channels/" + id)
+			return $http.delete("http://dummyapi.kodalagom.se/api/channels/" + id)
 				.then(
 					function (response) {
 						return response.data;
 					});
 		};
+		var addMessage = function (id, username, body) {
+			var data = {
+				'author': username,
+				'body': body,
+				'channelId': id
+			};
+			return $http.post(repositoryurl+"messages",data)
+				.then(function (response) {
+					return response.data;
+				});
+		};
 		return {
 			getAllChannels: getAllChannels,
 			getMessages: getMessages,
 			addChannel: addChannel,
-			deleteChannel : deleteChannel
+			deleteChannel: deleteChannel,
+			addMessage : addMessage
 		};
 	};
 	var module = angular.module("lerniaChat");
-	module.factory("repository", ["$http","$q", repository]);
+	module.factory("repository", ["$http", repository]);
 }());
